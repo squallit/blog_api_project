3ec5488af2eefe20cf0756499c4fd38fd1cde892
Paul Luu <paul.son.luu@gmail.com>
2019-01-18 22:47:46 -0800

    Added a simple APIView

--- src/blog_api_project/blog_api_app/views.py
+++ src/blog_api_project/blog_api_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 # Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            "User HTTP methods as function (get, post, patch, put, delete)",
+            "It is similar to a traditional Django view",
+            "Gives you the most control over your logic",
+            "is mapped manually to URLS"
+        ]
+
+        return Response({'message' : 'hello!', 'an_apiview': an_apiview})
