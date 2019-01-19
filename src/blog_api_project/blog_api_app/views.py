5c15c78a66d827f07c4f2f988f33bf922139f467
Paul Luu <paul.son.luu@gmail.com>
2019-01-19 00:20:28 -0800

    Added List, Create, Retrieve, Update, Partial Update, and Destroy to Viewsets

--- src/blog_api_project/blog_api_app/views.py
+++ src/blog_api_project/blog_api_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -45,3 +46,43 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
 
         return Response({'method' : 'delete'})
+
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+
+        a_viewset = [
+            "Use actions (list, create, retrieve, update, partial_update, destroy)",
+            "Automatically maps to URLs using Routers",
+            "Provides more functionality with less code."
+        ]
+
+        return Response({'message' : "Hello!", 'a_viewset' : a_viewset})
+
+    def create(self, request):
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({'message' : message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({'http_method' : "GET"})
+
+    def update(self, request, pk=None):
+        return Response({'http_method' : "PUT"})
+
+    def partial_update(self, request, pk=None):
+            return Response({'http_method' : "PATCH"})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method' : "DELETE"})
