93af5998d50cff7527d080c400e1d37067acf28b
Paul Luu <paul.son.luu@gmail.com>
2019-01-18 23:47:13 -0800

    Added Serializers, Added Get, Post, Put, Patch, and Delete functions to APIView

--- src/blog_api_project/blog_api_app/views.py
+++ src/blog_api_project/blog_api_app/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
 
@@ -18,3 +22,26 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message' : 'hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({'message' : message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+
+        return Response({'method' : 'put'})
+
+    def patch(self, request, pk=None):
+
+        return Response({'method' : 'patch'})
+
+    def delete(self, request, pk=None):
+
+        return Response({'method' : 'delete'})
