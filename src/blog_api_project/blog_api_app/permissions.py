72962d1fddaef4bab54bd0700853ec5475e7594f
Paul Luu <paul.son.luu@gmail.com>
2019-02-06 14:51:11 -0800

    API Project Completed

--- src/blog_api_project/blog_api_app/permissions.py
+++ src/blog_api_project/blog_api_app/permissions.py
@@ -13,4 +13,14 @@ class UpdateOwnProfile(permissions.BasePermission):
         # if Method is not safe such as UPDATE, DELETE
         # check if the currently authenticated user is the profile he is trying to change
         return obj.id == request.user.id
-        
+
+class PostOwnStatus(permissions.BasePermission):
+    # Update only user own status
+
+    def has_object_permission(self, request, view, obj):
+        # Check if user update their own status_text
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
